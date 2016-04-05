06b535379b5473429bf3d53c7db372f74a028b2b
kshitijtayal <kshitijtayal@gmail.com>
2016-04-05 12:16:18 +0530

    Added files via upload

---
+++ MinHeap.py
@@ -0,0 +1,54 @@
+class MinHeap:
+    
+    def __init__(self):
+        self.heapList = [0]     # heap List
+        self.currentSize = 0    # size of heap
+    
+    def percUp(self,i):          # percolate mininum to top in case of insertion
+        while i // 2 > 0:
+          if self.heapList[i] < self.heapList[i // 2]:
+             tmp = self.heapList[i // 2]
+             self.heapList[i // 2] = self.heapList[i]
+             self.heapList[i] = tmp
+          i = i // 2
+    
+    def insert(self,k):            # insert element 
+        self.heapList.append(k)
+        self.currentSize = self.currentSize + 1
+        self.percUp(self.currentSize)
+        
+    def percDown(self,i):               # percolate maximum downwards in case of replacement with minChild in case of deletion
+        while (i * 2) <= self.currentSize:
+            mc = self.minChild(i)
+            if self.heapList[i] > self.heapList[mc]:
+                tmp = self.heapList[i]
+                self.heapList[i] = self.heapList[mc]
+                self.heapList[mc] = tmp
+            i = mc
+
+    def minChild(self,i):             # choose minimum element to be replaced with deleted minimum
+        if i * 2 + 1 > self.currentSize:
+            return i * 2
+        else:
+            if self.heapList[i*2] < self.heapList[i*2+1]:
+                return i * 2
+            else:
+                return i * 2 + 1
+            
+    def delMin(self):                                   # delete minimum element from heap
+        retval = self.heapList[1]
+        self.heapList[1] = self.heapList[self.currentSize]
+        self.currentSize = self.currentSize - 1
+        self.heapList.pop()
+        self.percDown(1)
+        return retval
+    
+    def buildHeap(self,alist):        
+        i = len(alist) // 2
+        self.currentSize = len(alist)
+        self.heapList = [0] + alist[:]
+        while (i > 0):
+            self.percDown(i)
+            i = i - 1
+
+
